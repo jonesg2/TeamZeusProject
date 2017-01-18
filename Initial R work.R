5aa414f9367fa3cf729c9731f6bf0cbdbd26d539
jonesg2 <gareth.l.jones@ons.gov.uk>
2017-01-18 14:29:42 +0000

    Update Initial R work.R

--- Initial R work.R
+++ Initial R work.R
@@ -1,5 +1,6 @@
 #Import Data set
-DR2001to2012 <- read.csv("Z:/Team Zeus Project/Data Sources/DR2001to2012.csv")
+DR2001to2012 <- read.csv("Z:/Team Zeus Project/Data Sources/DR2001to2012.csv") 
+#please check which letter your shared drive is mapped to. If it is not Z then you will need to change the above code to the letter your have mapped yours to. 
 
 #create data frame
 DFDR2001to2012<-data.frame(DR2001to2012)
@@ -54,7 +55,8 @@ plot(AggregateDR2001to2021.subClungYRNDTHS$Group.1, AggregateDR2001to2021.subClu
 
 colnames(AggregateDR2001to2021.subClungYRNDTHS)<-c("YEAR", "NDTHS")
 
-#------------------------------------------------
+#--PLOTTING TOTAL NUMBER OF PREMATURE DEATHS
+
 #creating an age restricted data set for all registered deaths.
 #Removal of Age 75+ results
 DR2001to2012.age<-DR2001to2012[DR2001to2012$AGE != "75-79", ]
@@ -81,7 +83,7 @@ plot(AggregateDR2001to2012.agelungYRNDTHS$Group.1, AggregateDR2001to2012.agelung
 colnames(AggregateDR2001to2012.agelungYRNDTHS)<-c("YEAR", "NDTHS")
 
 
-#------------------------------------------------
+#---PLOTTING WHAT PROPORTION OF DEATHS REGISTERED AS LUNG CANCER
 
 #merge total deaths and lung cancer deaths
 DR2001to2012.merge1<-merge(AggregateDR2001to2021.subClungYRNDTHS, AggregateDR2001to2012.agelungYRNDTHS, by="YEAR")
@@ -99,10 +101,11 @@ plot(DR2001to2012.merge1$YEAR, DR2001to2012.merge1$PROPDTHS,
      ylab = "Proportion of Deaths",
      main = "Proportion of Deaths with cause of death is Lung Cancer")
 
-#-----------------------------------------
+#---PLOTTING LUNG CANCER DEATHS AS PROPORTION OF POPULATION
 
 #Import Populations Data set
 Population2001to2015 <- read.csv("Z:/Team Zeus Project/Data Sources/Population2001to2015.csv")
+#Please check which letter your shared drive is mapped to. If it is not Z then you will need to change the above code to the letter your have mapped yours to. 
 
 #Merge population stats with previous aggregate
 DR2001to2012.merge2<- merge(DR2001to2012.merge1, Population2001to2015, by = "YEAR")
