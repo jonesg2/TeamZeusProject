c909b608927df7a8d6b5c5921453d470ab71a878
jonesg2 <gareth.l.jones@ons.gov.uk>
2017-02-03 08:32:50 +0000

    Updated Naming convention
    Updated Naming convention
    
    fix issue #2

--- LungCancer Time Series Forecast.R
+++ LungCancer Time Series Forecast.R
@@ -1,43 +1,51 @@
-LungCancerDeaths <-read.csv("Z:/Team Zeus Project/Data Sources/Lung cancer deaths by month and region.csv")
+### Abbreviations ###
+# LCD = Lung Cancer Deaths
+# AG_LCD = Aggregate on Lung Cancer Deaths
+# TS_LCD = Time Series on Lung Cancer Deaths
+# FS_LCD = Forcast Series on Luncg Cancer Deaths
+# F24_LCD = 24 months forcast on Lung Cancer Deaths
+
+LCD <-read.csv("Z:/Team Zeus Project/Data Sources/Lung cancer deaths by month and region.csv")
 
 library(plyr)
 
 ## Turn month into number - JAY
-revalue(LungCancerDeaths$Group.1, c("January" = "01")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("February" = "02")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("March" = "03")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("April" = "04")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("May" = "05")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("June" = "06")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("July" = "07")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("August" = "08")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("September" = "09")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("October" = "10")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("November" = "11")) -> aggLCD$Group.1
-revalue(LungCancerDeaths$Group.1, c("December" = "12")) -> aggLCD$Group.1
+revalue(LCD$Month, c("January" = "01")) -> LCD$Month
+revalue(LCD$Month, c("February" = "02")) -> LCD$Month
+revalue(LCD$Month, c("March" = "03")) -> LCD$Month
+revalue(LCD$Month, c("April" = "04")) -> LCD$Month
+revalue(LCD$Month, c("May" = "05")) -> LCD$Month
+revalue(LCD$Month, c("June" = "06")) -> LCD$Month
+revalue(LCD$Month, c("July" = "07")) -> LCD$Month
+revalue(LCD$Month, c("August" = "08")) -> LCD$Month
+revalue(LCD$Month, c("September" = "09")) -> LCD$Month
+revalue(LCD$Month, c("October" = "10")) -> LCD$Month
+revalue(LCD$Month, c("November" = "11")) -> LCD$Month
+revalue(LCD$Month, c("December" = "12")) -> LCD$Month
 
 #combine - GARETH
-aggLCD$Date <- paste(aggLCD$Group.2,aggLCD$Group.1,sep="-")
+LCD$Date <- paste(LCD$Year,LCD$Month,sep="-")
 
-aggLCD <- aggregate(LungCancerDeaths$Deaths~LungCancerDeaths$Date, FUN = sum)
-aggLCD
+#Aggregate by Date to remove region split
+AG_LCD <- aggregate(LCD$Deaths~LCD$Date, FUN = sum)
+AG_LCD
 
 #add date reference - GARETH
-aggLCD$Date.reference<-seq(1,168,by=1)
+AG_LCD$Date.reference<-seq(1,168,by=1)
 
 ## PLOT the Time Series - GARETH
-df_Time_series<-ts(aggLCD$`LungCancerDeaths$Deaths`)
-plot(df_Time_series, xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths. Jan 2001-Dec 2015")
+TS_LCD<-ts(AG_LCD$`LCD$Deaths`)
+plot(TS_LCD, xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths. Jan 2001-Dec 2015")
 
 ##use simple exponential smoothing to make forecasts for the time series - ALEX
-LCDseriesforecasts <- HoltWinters(df_Time_series, beta=FALSE, gamma=FALSE, l.start=1318.374)
-LCDseriesforecasts
+FS_LCD <- HoltWinters(TS_LCD, beta=FALSE, gamma=FALSE, l.start=1318.374)
+FS_LCD
 #NOTE: The output of HoltWinters() tells us that the estimated value of the alpha parameter is about 0.132
 #This is close to zero, telling us that the forecasts are based on both recent and less recent observations.
 #By default, HoltWinters() just makes forecasts for the same time period covered by our original time series.
 
 ##PLOT exponential smoothing against original - ALEX
-plot(LCDseriesforecasts,xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths with forecast for same period")
+plot(FS_LCD,xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths with forecast for same period")
 
 ##Install "forecast" package & "ggplot2"
 install.packages("forecast")
@@ -45,10 +53,8 @@ library("forecast")
 library("ggplot2")
 
 ##Forecast for next 24 months (Jan2015-Dec2016) 
-LCDforecast24 <- forecast.HoltWinters(LCDseriesforecasts, h=24)
+F24_LCD <- forecast.HoltWinters(FS_LCD, h=24)
 ##Plot forecast2 against original 
-plot(LCDforecast24,xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths Jan 2001-Dec 2030", col="blue")
+plot(F24_LCD,xlab="Time in Months", ylab="Number Of Deaths", main="Number of Lung Cancer Deaths Jan 2001-Dec 2030", col="blue")
 #Here the forecasts for Jan2015-Dec2016 are plotted as a blue line,
 #the 80% prediction interval as the inner shaded area, and the 95% prediction interval as the outer shaded area
-
-
